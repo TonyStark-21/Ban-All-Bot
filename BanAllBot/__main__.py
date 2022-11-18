c3084833547e4e8c3d8be0db1e8e7385f7dda94b
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-18 20:17:03 +0530

    Create __main__.py

---
+++ BanAllBot/__main__.py
@@ -0,0 +1,10 @@
+from BanAllBot import app
+from pyrogram import filters
+
+@app.on_message(filters.command("start"))
+async def start(_,msg):
+    await msg.reply_photo("i am under creation by @NoobStark-21")
+
+if __name__ == "__main__":
+    print("started")
+    app.run() 
