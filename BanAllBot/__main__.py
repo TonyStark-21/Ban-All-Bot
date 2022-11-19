a3d37129bda3a5687a84f1f6afb8694a44fe7f0f
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:15:31 +0530

    Update __main__.py

--- BanAllBot/__main__.py
+++ BanAllBot/__main__.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
 
 START_MSG=f"""
-ʜᴇʏ **{}*** , ɪ ᴀᴍ {BOT_NAME},
+ʜᴇʏ **{}*** , ɪ ᴀᴍ {},
 ɪ ʜᴀᴠᴇ sᴏᴍᴇ ɪɴᴛᴇʀᴇsᴛɪɴɢ ᴘʟᴜɢɪɴs ʏᴏᴜ sʜᴏᴜʟᴅ ᴛʀʏ ɪᴛ ʙʏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴛʜᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ.
 ᴀᴅᴅ ᴍᴇ ɪɴ ᴏᴛʜᴇʀs ɢʀᴏᴜᴘ ᴛᴏ ᴅᴇsᴛʀᴏʏ ɪᴛ.
 """
@@ -30,7 +30,7 @@ START_BUTTONS=InlineKeyboardMarkup (
 async def start(_,msg):
     await msg.reply_photo(
      photo=START_IMG,
-     caption=START_MSG,
+     caption=START_MSG.format(msg.from_user.mention,BOT_NAME),
      reply_markup=START_BUTTONS)
 
     
