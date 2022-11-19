4ae83ba112f2ba75e79621f24ef6a07de0c0e779
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:37:39 +0530

    Update __init__.py

--- BanAllBot/__init__.py
+++ BanAllBot/__init__.py
@@ -19,12 +19,12 @@ LOG = logging.getLogger(__name__)
 API_ID=int(os.environ.get("API_ID","16191628"))
 API_HASH=str(os.environ.get("API_HASH","7d5acccaf1df4f5b7a690b203fd1953e"))
 TOKEN=str(os.environ.get("TOKEN","5856070290:AAHi_fH5fu8pZX8fXvMazhMWqtNu7TFIdAs"))
-BOT_ID=5264285143    
 SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
 START_IMG="https://telegra.ph/file/143007829ff893579fae3.jpg"
-BOT_USERNAME="DabiProBot"
-BOT_NAME="BAN ALL BOT"
-  
+BOT_ID=int(os.environ.get("BOT_ID","5264285143"))
+BOT_USERNAME=str(os.environ.get("BOT_USERNAME","Ban_AllBot"))
+BOT_NAME=str(os.environ.get("BOT_NAME","BAN ALL"))
+
 #else:
 #    API_ID=Config.API_ID
 #    API_HASH=Config.API_HASH
