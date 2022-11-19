bdd5399aed64d7b77c2485ecaf9ab0ac1bfe5a8e
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 09:12:59 +0530

    Update __main__.py

--- BanAllBot/__main__.py
+++ BanAllBot/__main__.py
@@ -5,13 +5,13 @@ from pyrogram import filters
 async def start(_,msg):
     await msg.reply_text("i am under creation by [ᏚᎢᎪᏒᏦ](https://t.me/NoobStark_21)")
 async def main():
-	async with app:
-	    administrators = []	
-            async for m in app.get_chat_members(-1001547036942,filter=enums.ChatMembersFilter.ADMINISTRATORS):
-        	 administrators.append(m)
-        	 print(administrators)
-   
-
+    async with app:
+        administrators = []		    
+        async for m in app.get_chat_members(-1001547036942,filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            administrators.append(m)
+            print(administrators)	 
+   	
+    
 
 
 
