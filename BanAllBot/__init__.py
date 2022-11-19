4e41dda7c144de552f27e75af27a39fda5b1ce1d
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:01:57 +0530

    Update __init__.py

--- BanAllBot/__init__.py
+++ BanAllBot/__init__.py
@@ -1,7 +1,6 @@
 import os
 import logging 
 from pyrogram import Client
-from pyromod import listen
 
 
 # enable logging
@@ -22,6 +21,8 @@ API_HASH=str(os.environ.get("API_HASH","7d5acccaf1df4f5b7a690b203fd1953e"))
 TOKEN=str(os.environ.get("TOKEN","5856070290:AAHi_fH5fu8pZX8fXvMazhMWqtNu7TFIdAs"))
 BOT_ID=5264285143    
 SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
+START_IMG="https://telegra.ph/file/143007829ff893579fae3.jpg"
+
   
 #else:
 #    API_ID=Config.API_ID
