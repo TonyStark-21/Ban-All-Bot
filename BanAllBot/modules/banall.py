c0682fc8de6564dc6573ae337d3022326c262183
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 07:53:44 +0530

    Update banall.py

--- BanAllBot/modules/banall.py
+++ BanAllBot/modules/banall.py
@@ -1,1 +1,12 @@
+from BanAllBot import app
+from pyrogram import filters
 
+
+@app.on_message(filters.command("banall")
+async def ban_all(_,msg):
+    chat_id=msg.chat.id
+    bot=await app.get_chat_member(chat_id,BOT_ID)
+    permission=bot.privileges.can_restrict_members==True
+    async for member in app.get_chat_members(chat_id):
+        print(member.user.id)
+    
