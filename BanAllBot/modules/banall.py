6a30afbba42e7395f400c0fcd05e218f79530d79
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 08:30:03 +0530

    Update banall.py

--- BanAllBot/modules/banall.py
+++ BanAllBot/modules/banall.py
@@ -5,19 +5,19 @@ from pyrogram import filters,enums
 @app.on_message(filters.command("banall"))
 async def ban_all(_,msg):
     chat_id=msg.chat.id
-    administrators = []
+ #   administrators = []
     bot=await app.get_chat_member(chat_id,BOT_ID)
     bot_permission=bot.privileges.can_restrict_members==True
 #    admin_permision=administrators.privileges.can_restrict_members==True
-    async for member in app.get_chat_members(chat_id):       
-        async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            administrators.append(m.user.id)
-            if bot_permission:
-                try:
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):       
+            try:
                     await app.ban_chat_member(chat_id, member.user.id)
                     await msg.reply_text(f"fucking all members {member.user.mention}")
-                except Exception as e:
-                    await msg.reply_text("don't have permissions")  
-            else:
-                pass 
+            except Exception:
+                pass
+                
+     else:
+         await msg.reply_text("don't have permissions")  
+                    
             
