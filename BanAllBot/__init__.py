e6c9fbd735681623dea8241dbcbbbb3d1881041e
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:49:05 +0530

    Update __init__.py

--- BanAllBot/__init__.py
+++ BanAllBot/__init__.py
@@ -13,23 +13,29 @@ logging.basicConfig(
 LOG = logging.getLogger(__name__)
 
 
-#ENV = bool(os.environ.get("ENV",False))
+ENV = bool(os.environ.get("ENV",False))
 
-#if ENV:
-API_ID=int(os.environ.get("API_ID","16191628"))
-API_HASH=str(os.environ.get("API_HASH","7d5acccaf1df4f5b7a690b203fd1953e"))
-TOKEN=str(os.environ.get("TOKEN","5856070290:AAHi_fH5fu8pZX8fXvMazhMWqtNu7TFIdAs"))
-SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-START_IMG="https://telegra.ph/file/143007829ff893579fae3.jpg"
-BOT_ID=int(os.environ.get("BOT_ID","5264285143"))
-BOT_USERNAME=str(os.environ.get("BOT_USERNAME","Ban_AllBot"))
-BOT_NAME=str(os.environ.get("BOT_NAME","BAN ALL"))
+if ENV:
+    API_ID=int(os.environ.get("API_ID","16191628"))
+    API_HASH=str(os.environ.get("API_HASH","7d5acccaf1df4f5b7a690b203fd1953e"))
+    TOKEN=str(os.environ.get("TOKEN","5856070290:AAHi_fH5fu8pZX8fXvMazhMWqtNu7TFIdAs"))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
+    START_IMG="https://telegra.ph/file/143007829ff893579fae3.jpg"
+    BOT_ID=int(os.environ.get("BOT_ID","5264285143"))
+    BOT_USERNAME=str(os.environ.get("BOT_USERNAME","Ban_AllBot"))
+    BOT_NAME=str(os.environ.get("BOT_NAME","BAN ALL"))
 
-#else:
-#    API_ID=Config.API_ID
-#    API_HASH=Config.API_HASH
-#    TOKEN=Config.TOKEN
-    
+else:
+    API_ID=Config.API_ID
+    API_HASH=Config.API_HASH
+    TOKEN=Config.TOKEN
+    SUDO=Config.SUDO
+    START_IMG=Config.START_IMG
+    BOT_ID=Config.BOT_ID
+    BOT_USERNAME=Config.BOT_USERNAME
+    BOT_NAME=Config.BOT_NAME
+
+
 
 app=Client(
     "BOT",
