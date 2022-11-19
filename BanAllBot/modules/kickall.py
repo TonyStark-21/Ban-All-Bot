11b2bf6cce2ada0c8c39cf15df81a5d4a4fb2b98
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 09:45:50 +0530

    Update kickall.py

--- BanAllBot/modules/kickall.py
+++ BanAllBot/modules/kickall.py
@@ -1,24 +1,22 @@
-from BanAllBot import app,BOT_ID
-from pyrogram import filters,enums
+from BanAllBot import app,BOT_ID,SUDO
+from pyrogram import filters
 
 
 @app.on_message(filters.command("kickall"))
 async def ban_all(_,msg):
-    chat_id=msg.chat.id
- #   administrators = []
+    chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
-    bot_permission=bot.privileges.can_restrict_members==True
-#    admin_permision=administrators.privileges.can_restrict_members==True
-    if bot_permission:
+    bot_permission=bot.privileges.can_restrict_members==True    
+    if bot_permission and msg.from_user.id in SUDO:
         async for member in app.get_chat_members(chat_id):       
             try:
                     await app.ban_chat_member(chat_id, member.user.id)
-                    await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")
-                    await app.unban_chat_member(chat_id, member.user.id)
+                    await msg.reply_text(f"ᴋɪᴄᴋɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")
+                    await app.unban_chat_member(chat_id,member.user.id)                    
             except Exception:
                 pass
     else:
-        await msg.reply_text("don't have permissions")  
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
                                          
-     
+    
             
