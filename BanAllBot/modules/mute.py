15d427b0674ad778a468479a14ec24f1ebd00e88
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 11:01:34 +0530

    Update mute.py

--- BanAllBot/modules/mute.py
+++ BanAllBot/modules/mute.py
@@ -1,8 +1,23 @@
-from BanAllBot import app
+from BanAllBot import app,BOT_ID,SUDO
 from pyrogram import filters
 from pyrogram.types import ChatPermissions
 
 
-@app.on_message(filters.command("mute"))
-async def mute(_,msg):
-    await app.restrict_chat_member(-1001547036942,5715447071,ChatPermissions(can_send_messages=False))
+@app.on_message(filters.command("kickall") & filters.user(SUDO))
+async def ban_all(_,msg):
+    chat_id=msg.chat.id    
+    bot=await app.get_chat_member(chat_id,BOT_ID)
+    bot_permission=bot.privileges.can_restrict_members==True    
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):       
+            try:
+                    await app.restrict_chat_member(chat_id, member.user.id,ChatPermissions(can_send_messages=False))
+                    await msg.reply_text(f"ᴋɪᴄᴋɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")
+                                        
+            except Exception:
+                pass
+    else:
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
+                                         
+    
+            
