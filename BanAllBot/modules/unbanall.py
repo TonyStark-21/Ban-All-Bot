700cc0244ed68595952949a0f664f32d99aac371
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 10:21:37 +0530

    Update unbanall.py

--- BanAllBot/modules/unbanall.py
+++ BanAllBot/modules/unbanall.py
@@ -10,13 +10,15 @@ async def unban_all(_,msg):
     if bot_permission:
         banned_users = []
         async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
-            banned_users.append(m.user.id)       
-            try:
-                    await app.unban_chat_member(chat_id,banned_users[0])
+            banned_users.append(m.user.id)  
+            for i in range(len[banned_users]:
+                try:
+                    await app.unban_chat_member(chat_id,i)
                     await msg.reply_text(f"unbannig ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {m.user.mention}")
                                         
-            except Exception:
-                pass
+                except Exception:
+                pass    
+            
     else:
         await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
                                          
