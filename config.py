b44c658f19fb14be6838924a577721c9f08505e8
sᴛᴀʀᴋ ɪɴᴅᴜsᴛʀʏ <115603133+TonyStark-21@users.noreply.github.com>
2022-11-19 16:30:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     TOKEN="jaksks"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
     START_IMG="jdjsjw"
-    BOT_ID=sjsja
+    BOT_ID=1013
     BOT_USERNAME="djsjw"
     BOT_NAME="BAN ALL"
