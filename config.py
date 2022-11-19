24d91b4cca01e4af5dc1af5d77b1ea035eee6cdd
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:52:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    API_ID=16191628
+    API_HASH="7d5acccaf1df4f5b7a690b203fd1953e"
+    TOKEN="5856070290:AAHi_fH5fu8pZX8fXvMazhMWqtNu7TFIdAs"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
+    START_IMG="https://telegra.ph/file/143007829ff893579fae3.jpg"
+    BOT_ID=5264285143
+    BOT_USERNAME="Ban_AllBot"
+    BOT_NAME="BAN ALL"
