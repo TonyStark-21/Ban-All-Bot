f6c8cd3a15c07036de6eaa7c19943b80934a72a3
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 10:55:16 +0530

    Update mute.py

--- BanAllBot/modules/mute.py
+++ BanAllBot/modules/mute.py
@@ -4,4 +4,4 @@ from pyrogram import filters
 
 @app.on_message(filters.command("mute"))
 async def mute(_,msg):
-    await app.restrict_chat_member(-1001547036942,5715447071)
+    await app.restrict_chat_member(-1001547036942,5715447071,ChatPermissions(can_send_messages=True))
