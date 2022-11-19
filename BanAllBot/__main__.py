c78b54d58ed01ece821f277ce6173a840acc3b7a
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:26:55 +0530

    Update __main__.py

--- BanAllBot/__main__.py
+++ BanAllBot/__main__.py
@@ -16,14 +16,27 @@ START_BUTTONS=InlineKeyboardMarkup (
          InlineKeyboardButton (text="ʜᴇʟᴘ",callback_data="help_back")
       ]
       ]
-
 )
 
-#HELP_MSG="""
+HELP_MSG="""
+**ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ɪɴ ɢʀᴏᴜᴘs**
 
-#"""
+⨷ /banall : ʙᴀɴ-ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ᴀ ɢʀᴏᴜᴘ
 
-#HELP_BUTTONS=
+⨷ /unbanall : ᴜɴʙᴀɴ ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ᴀ ɢʀᴏᴜᴘ
+
+⨷ /kickall : ᴋɪᴄᴋ ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ᴀ ɢʀᴏᴜᴘ
+
+⨷ /muteall : ᴍᴜᴛᴇ ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ᴀ ɢʀᴏᴜᴘ
+
+⨷ /unmuteall : ᴜɴᴍᴜᴛᴇ ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ᴀ ɢʀᴏᴜᴘ(sᴛɪʟʟ ᴡɪʟʟ ᴛʜᴇ ʟɪsᴛ ɪɴ ʀᴇsᴛʀɪᴄᴛᴇᴅ ᴍᴇᴍʙᴇʀs ʙᴜᴛ ᴀʟʟ ʀᴇsᴛʀɪᴄᴛɪᴏɴs ᴡɪʟʟ ɢᴏ)
+
+⨷/unpinall : ᴜɴᴘɪɴ ᴀʟʟ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ɢʀᴏᴜᴘ.
+
+ᴄʀᴇᴀᴛᴇᴅ ʙʏ: [sᴛᴀʀᴋ ɪɴᴅᴜsᴛʀʏ](https://t.me/StarkIndustry_21)
+"""
+
+
 
 
 @app.on_message(filters.command("start"))
@@ -33,7 +46,10 @@ async def start(_,msg):
      caption=START_MSG.format(msg.from_user.mention,BOT_NAME),
      reply_markup=START_BUTTONS)
 
-    
+@app.on_callback_query(filters.regex("help_back"))
+async def help_back(_,callback_query: CallbackQuery):
+    query=callback_query.message
+    await query.edit_caption(HELP_MSG)    
 
 
 
