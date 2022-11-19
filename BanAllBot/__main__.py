935fa23f39322e148182dcd584ba0f55be65b8d9
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 09:11:45 +0530

    Update __main__.py

--- BanAllBot/__main__.py
+++ BanAllBot/__main__.py
@@ -6,8 +6,8 @@ async def start(_,msg):
     await msg.reply_text("i am under creation by [ᏚᎢᎪᏒᏦ](https://t.me/NoobStark_21)")
 async def main():
 	async with app:
-		administrators = []
-        async for m in app. get_chat_members(-1001547036942, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+	    administrators = []	
+            async for m in app.get_chat_members(-1001547036942,filter=enums.ChatMembersFilter.ADMINISTRATORS):
         	 administrators.append(m)
         	 print(administrators)
    
