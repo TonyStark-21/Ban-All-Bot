d8096e1526df4d70c5f8401bbdd964a919f21453
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:10:52 +0530

    Update __main__.py

--- BanAllBot/__main__.py
+++ BanAllBot/__main__.py
@@ -1,9 +1,37 @@
-from BanAllBot import app
-from pyrogram import filters,enums
+from BanAllBot import app,START_IMG,BOT_USERNAME,BOT_NAME
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+
+START_MSG="""
+ʜᴇʏ **{}*** , ɪ ᴀᴍ ʙᴀɴ ᴀʟʟ ʙᴏᴛ,
+ɪ ʜᴀᴠᴇ sᴏᴍᴇ ɪɴᴛᴇʀᴇsᴛɪɴɢ ᴘʟᴜɢɪɴs ʏᴏᴜ sʜᴏᴜʟᴅ ᴛʀʏ ɪᴛ ʙʏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴛʜᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ.
+ᴀᴅᴅ ᴍᴇ ɪɴ ᴏᴛʜᴇʀs ɢʀᴏᴜᴘ ᴛᴏ ᴅᴇsᴛʀᴏʏ ɪᴛ.
+"""
+START_BUTTONS=InlineKeyboardMarkup (
+      [
+      [
+         InlineKeyboardButton (text="➕ ᴀᴅᴅ ᴍᴇ ➕",url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+      ],
+      [
+         InlineKeyboardButton (text="ʜᴇʟᴘ",callback_data="help_back")
+      ]
+      ]
+
+)
+
+HELP_MSG="""
+
+"""
+
+HELP_BUTTONS=
+
 
 @app.on_message(filters.command("start"))
 async def start(_,msg):
-    await msg.reply_text("i am under creation by [ᏚᎢᎪᏒᏦ](https://t.me/NoobStark_21)")
+    await msg.reply_photo(
+     photo=START_IMG,
+     
+"i am under creation by [ᏚᎢᎪᏒᏦ](https://t.me/NoobStark_21)")
 
     
 
