06178e5281b6ef1a74e27341779a55c728b8f727
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:31:57 +0530

    Update mute.py

--- BanAllBot/modules/mute.py
+++ BanAllBot/modules/mute.py
@@ -4,7 +4,7 @@ from pyrogram.types import ChatPermissions
 
 
 @app.on_message(filters.command("muteall") & filters.user(SUDO))
-async def ban_all(_,msg):
+async def mute_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
     bot_permission=bot.privileges.can_restrict_members==True    
@@ -12,7 +12,7 @@ async def ban_all(_,msg):
         async for member in app.get_chat_members(chat_id):       
             try:
                     await app.restrict_chat_member(chat_id, member.user.id,ChatPermissions(can_send_messages=False))
-                    await msg.reply_text(f"ᴋɪᴄᴋɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")
+                    await msg.reply_text(f"ᴍᴜᴛɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs {member.user.mention}")
                                         
             except Exception:
                 pass
