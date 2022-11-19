534c89308e0387da5e24910db72a0c24759bc22c
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 12:52:52 +0530

    Update __init__.py

--- BanAllBot/__init__.py
+++ BanAllBot/__init__.py
@@ -20,7 +20,7 @@ if ENV:
     API_HASH=str(os.environ.get("API_HASH","7d5acccaf1df4f5b7a690b203fd1953e"))
     TOKEN=str(os.environ.get("TOKEN","5856070290:AAHi_fH5fu8pZX8fXvMazhMWqtNu7TFIdAs"))
     SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-    START_IMG="https://telegra.ph/file/143007829ff893579fae3.jpg"
+    START_IMG=str(os.environ.get("START_IMG","https://telegra.ph/file/143007829ff893579fae3.jpg"))
     BOT_ID=int(os.environ.get("BOT_ID","5264285143"))
     BOT_USERNAME=str(os.environ.get("BOT_USERNAME","Ban_AllBot"))
     BOT_NAME=str(os.environ.get("BOT_NAME","BAN ALL"))
