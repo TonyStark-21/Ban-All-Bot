08c7325710fc27894c73d5534ecd4f6ba3be5340
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 10:02:39 +0530

    Update banall.py

--- BanAllBot/modules/banall.py
+++ BanAllBot/modules/banall.py
@@ -2,12 +2,12 @@ from BanAllBot import app,BOT_ID,SUDO
 from pyrogram import filters,enums
 
 
-@app.on_message(filters.command("banall"))
+@app.on_message(filters.command("banall") & filters.user(SUDO))
 async def ban_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
     bot_permission=bot.privileges.can_restrict_members==True    
-    if bot_permission and msg.from_user.id in SUDO:
+    if bot_permission:
         async for member in app.get_chat_members(chat_id):       
             try:
                     await app.ban_chat_member(chat_id, member.user.id)
