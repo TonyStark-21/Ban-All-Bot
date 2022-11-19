aaa444f637ffd31872fd44adc5bc57dc9f186dce
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-19 11:28:55 +0530

    Update pinall.py

--- BanAllBot/modules/pinall.py
+++ BanAllBot/modules/pinall.py
@@ -3,5 +3,14 @@ from pyrogram import filters
 
 @app.on_message(filters.command("unpinall") & filters.user(SUDO))
 async def unpin_all(_,msg):
-    chat_id=msg.chat.id
-    await app.unpin_all_chat_messages(chat_id)
+    chat_id=msg.chat.id    
+    bot=await app.get_chat_member(chat_id,BOT_ID)
+    bot_permission=bot.privileges.can_pin_messages==True
+    if bot_permission:
+        try:
+            await app.unpin_all_chat_messages(chat_id)
+            await msg.reply_text("unpinned all")
+        except Exception:
+            pass
+    else:
+        await msg.reply_text("don't have permissions")
